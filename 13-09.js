e4709f88a95c13695ec65a1280ba966c82de6e42
thainarajanuario <gigitata199427@gmail.com>
2022-09-15 11:25:18 -0300

    Exercicios13-09

---
+++ 13-09.js
@@ -0,0 +1,154 @@
+// For // Para
+// While // enquanto
+// do-While // faça enquanto
+
+
+//for (inicialização; condição; incremento/decremento)
+
+//alert (Sua bomba vai explodir: );
+//for (i = 0; i <= 10; i++){
+//     alert (i)
+//}
+//alert("Sua bomba explodiu!")
+
+
+
+//let abacaxi = 0; //inicialização
+//while(abacaxi < 5){ //condição
+    //abacaxi ++ //incremento
+    //alert (abacaxi)
+
+//}
+
+
+
+//let i = 0;
+//do{
+    //alert(i);
+    //i++;
+//}while(i < 5);
+
+
+
+//exercicio1For
+
+/*let  filhos, sal100 = 0, salario, soma = 0, medsal = 0.0, mediaF, perc  
+
+for(i = 0; i <= 20; i++){
+    salario = prompt("Digite seu salario");
+    filhos = prompt("Digite o numero de filhos");
+
+
+    soma = parseFloat(soma) + parseFloat(salario);
+    mediaF = parseInt(mediaF) + parseInt(filhos)
+
+    if(msal < salario){
+        msal = parseFloat(salario)
+    }
+
+    if(salario <= 100){
+        parseFloat(sal100++)
+
+    }
+}
+
+medsal = soma / 20;
+mediaF = parseInt(mediaF) / 20;
+
+
+perc = (parseFloat(sal100)/ 20) * 100
+alert("A media do salario da população: " + medsal + "/nmedia de filhos da população é:" + mediaF + "/nO maior salario foi: " + msal + "/nO percentual de pessoas com salario até 100 é: " + perc )*/
+
+
+
+
+
+
+//exercicio2For
+
+/*let num, soma=0
+
+for (num = 1; num <= 500; num++ ){
+
+    if (num %2 != 0 && num %3 == 0)
+    soma += num 
+}
+alert ("A soma dos seus multiplos de 3, impares de 1 á 500 é " + soma)*/
+
+
+
+
+//exercicio1while
+
+/*let num = 0, soma = 0.0, media, total;
+    
+     while(num >= 0){
+        num = prompt("Digite o numero: ");
+
+        if(num >= 0){
+            soma = parseInt(soma) + parseInt(num)
+            total++
+
+        }
+     }
+     
+     media = soma / total;
+
+     alert("Soma de todos os numeros digitados: " + soma);
+     alert("media de todos os numeros digitados: " + media);
+     alert("total de todos os numeros digitados: " + total);*/
+
+
+
+     //exercicio2while
+
+
+    /* let num = 0, mult
+     num = prompt("digite o numero: ");
+
+     while(num <= 100){
+        num = num * 3
+
+        alert (num)
+     }*/
+
+
+     //exercicio1do-while
+
+
+     /*let cont = 233;
+
+     do{
+        if (cont >= 300 && cont <= 400){
+            cont += 3
+
+        }else{
+        cont += 5
+    }
+     alert(cont);
+     
+     }while(cont <= 456);*/
+
+
+
+     //exercicio2do-while
+
+    /* let num = 0, result
+
+     num = prompt("digite o numero: ");
+
+     do{
+        resultado = parseInt(resultado) + parseInt(num);
+        num--
+    
+     }while(num >= 0);
+      
+     alert(resultado)*/
+
+
+
+
+
+
+
+
